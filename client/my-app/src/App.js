d4f0259582fabde30e0eb6c43eb632bc269c44fc
Oussama Fellag <jo_fellag@esi.dz>
2024-02-02 10:31:28 +0100

    client side dev v2

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,18 +1,48 @@
+// import "./App.css";
+// import axios from "axios"; 
+// import { useEffect, useState } from "react";
+// function App() {
+// const [listOfPosts, setListOfPosts] = useState([]);
+// useEffect(() => {
+// axios.get("http://localhost:3001/posts").then((response) => {
+// setListOfPosts (response.data);
+// });
+// }, []);
+// return (
+// <div className="App">
+// {listOfPosts.map((value, key) => {
+// return <div> {value.title} </div>
+// })}
+// </div>
+// );
+// }
+
 import "./App.css";
 import axios from "axios"; 
-import { useEffect, useState } from "react";
+
+import { useEffect, useState } from "react"; 
 function App() {
-const [listOfPosts, setListOfPosts] = useState([]);
-useEffect(() => {
+
+    const [listOfPosts, setListOfPosts] = useState([]);
+
+useEffect (() => {
 axios.get("http://localhost:3001/posts").then((response) => {
-setListOfPosts (response.data);
+    setListOfPosts (response.data);
+
 });
 }, []);
 return (
-<div className="App">
-{listOfPosts.map((value, key) => {
-return <div> {value.title} </div>
-})}
-</div>
-);
+    <div className="App">
+    {listOfPosts.map((value, key) => {
+    return (
+        <div className="post">
+            <div className="title">  {value.title}  </div>
+            <div className="body">  {value.postText}  </div>
+            <div className="footer">  {value.username}  </div>
+        </div>
+        )
+    })}
+    </div>
+    );
 }
+export default App;
--- server/index.js
+++ server/index.js
@@ -11,3 +11,17 @@ db.sequelize.sync().then(() => {
         console.log("Server running on port 3001");     //print port server
     });
 });
+// const express = require("express");
+// const app = express(); 
+// const cors = require("cors"); 
+// app.use(express.json());
+// app.use(cors());
+// const db = require("./models"); 
+// // Routers
+// const postRouter =require("./routes/posts");
+// app.use("/posts", postRouter);
+// db.sequelize.sync().then(() => {
+// app.listen (3001, () => {
+// console.log("Server running on port 3001");
+// });
+// });
