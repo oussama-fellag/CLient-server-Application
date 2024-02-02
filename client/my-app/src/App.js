3e0f899dc6dafd32e0a1d44811b6bc9feeaa935c
Oussama Fellag <jo_fellag@esi.dz>
2024-02-02 09:19:23 +0100

    client side dev

---
+++ client/my-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import axios from "axios"; 
+import { useEffect, useState } from "react";
+function App() {
+const [listOfPosts, setListOfPosts] = useState([]);
+useEffect(() => {
+axios.get("http://localhost:3001/posts").then((response) => {
+setListOfPosts (response.data);
+});
+}, []);
+return (
+<div className="App">
+{listOfPosts.map((value, key) => {
+return <div> {value.title} </div>
+})}
+</div>
+);
+}
